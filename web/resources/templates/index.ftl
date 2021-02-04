a34fc3aa9ded6c2f3bcef67305f13e687900e594
Ellie <git@angm.xyz>
2021-02-04 12:38:16 +0100

    Add a template for website

---
+++ web/resources/templates/index.ftl
@@ -0,0 +1,23 @@
+<!--
+  ~ Developed as part of the PokeMMO project.
+  ~ This file was last modified at 2/4/21, 12:26 PM.
+  ~ Copyright 2020, see git repository at git.angm.xyz for authors and other info.
+  ~ This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
+  -->
+
+<#-- @ftlvariable name="time" type="java.lang.String" -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Nice</title>
+    <link href="/static/css/index.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<h1>Nice.</h1>
+<p>It is ${ time }.</p>
+</body>
+
+</html>
---
+++ web/src/xyz/angm/pokemmo.web/Application.kt
@@ -0,0 +1,41 @@
+/*
+ * Developed as part of the PokeMMO project.
+ * This file was last modified at 2/4/21, 12:36 PM.
+ * Copyright 2020, see git repository at git.angm.xyz for authors and other info.
+ * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
+ */
+
+package xyz.angm.pokemmo.web
+
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.freemarker.FreeMarker
+import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import java.time.LocalDateTime
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
+
+    routing {
+        static("/static") {
+            resources("static")
+        }
+
+        get("/") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("time" to LocalDateTime.now().toString())))
+        }
+    }
+}

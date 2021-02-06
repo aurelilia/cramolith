7d8051d3f780df8e1a5f70a8add8094753c9eeda
Ellie <git@angm.xyz>
2021-02-06 20:25:32 +0100

    Adjust options panel styling

--- core/src/xyz/angm/cramolith/client/graphics/panels/menu/options/OptionsPanel.kt
+++ core/src/xyz/angm/cramolith/client/graphics/panels/menu/options/OptionsPanel.kt
@@ -1,12 +1,13 @@
 /*
  * Developed as part of the Cramolith project.
- * This file was last modified at 2/6/21, 8:09 PM.
+ * This file was last modified at 2/6/21, 8:25 PM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
 
 package xyz.angm.cramolith.client.graphics.panels.menu.options
 
+import com.badlogic.gdx.utils.Align
 import ktx.actors.onChange
 import ktx.actors.onClick
 import ktx.actors.plusAssign
@@ -35,26 +36,23 @@ class OptionsPanel(screen: Screen, parent: MainMenuPanel? = null) : Panel(screen
             if (screen is MenuScreen) {
                 val box = visSelectBoxOf(I18N.languages())
                 box.selected = configuration.language
+                box.setAlignment(Align.center)
                 box.onChange {
                     I18N.setLanguage(box.selected)
                     parent!!.reload(screen)
                     reload(screen, parent)
                     this@OptionsPanel.isVisible = true // Regrab focus lost by reload
                 }
-                box.inCell.colspan(2)
+                box.inCell.height(Skin.textButtonHeight).width(Skin.textButtonWidth).pad(20f).row()
                 row()
             }
 
             visTextButton(I18N["options.controls"]) {
-                it.height(Skin.textButtonHeight).width(Skin.textButtonWidth).pad(20f).colspan(2).row()
+                it.height(Skin.textButtonHeight).width(Skin.textButtonWidth).pad(20f).row()
                 onClick { screen.pushPanel(ControlsPanel(screen)) }
             }
 
-            visTextButton(I18N["back"]) {
-                it.height(Skin.textButtonHeight).width(Skin.textButtonWidth).pad(20f).colspan(2)
-                onClick { screen.popPanel() }
-            }
-
+            backButton(screen)
             setFillParent(true)
         }
     }

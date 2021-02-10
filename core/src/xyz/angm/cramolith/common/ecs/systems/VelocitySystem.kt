9f422e669f854c1f921b0715a24dbc01bc6f2664
Ellie <git@angm.xyz>
2021-02-10 23:42:59 +0100

    Modify velocity handling

--- core/src/xyz/angm/cramolith/common/ecs/systems/VelocitySystem.kt
+++ core/src/xyz/angm/cramolith/common/ecs/systems/VelocitySystem.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Cramolith project.
- * This file was last modified at 2/4/21, 12:43 PM.
+ * This file was last modified at 2/10/21, 10:56 PM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -14,12 +14,14 @@ import xyz.angm.rox.Entity
 import xyz.angm.rox.Family
 import xyz.angm.rox.systems.IteratingSystem
 
+const val SPEED = 100f
+
 class VelocitySystem : IteratingSystem(Family.allOf(position, velocity)) {
     override fun process(entity: Entity, delta: Float) {
         val vel = entity[velocity]
         val pos = entity[position]
-        pos.x += vel.x
-        pos.y += vel.y
+        pos.x += vel.x * delta * SPEED
+        pos.y += vel.y * delta * SPEED
         entity.c(network)?.needsSync = true
     }
 }

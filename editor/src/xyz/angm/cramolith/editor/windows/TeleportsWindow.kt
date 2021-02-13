3fa424b69060816195de5177293f6fb857e5ad29
Ellie Ang <git@angm.xyz>
2021-02-13 02:36:02 +0100

    Teleport remove button

--- editor/src/xyz/angm/cramolith/editor/windows/TeleportsWindow.kt
+++ editor/src/xyz/angm/cramolith/editor/windows/TeleportsWindow.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Cramolith project.
- * This file was last modified at 2/11/21, 11:21 PM.
+ * This file was last modified at 2/13/21, 2:34 AM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -10,8 +10,11 @@ package xyz.angm.cramolith.editor.windows
 import com.kotcrab.vis.ui.util.dialog.Dialogs
 import com.kotcrab.vis.ui.util.dialog.InputDialogAdapter
 import com.kotcrab.vis.ui.widget.VisLabel
+import com.kotcrab.vis.ui.widget.VisTextButton
+import ktx.actors.onClick
 import xyz.angm.cramolith.client.graphics.panels.textBtn
 import xyz.angm.cramolith.common.world.Teleport
+import xyz.angm.cramolith.common.world.TriggerType
 import xyz.angm.cramolith.common.world.WorldMap
 import xyz.angm.cramolith.editor.EditorScreen
 
@@ -24,9 +27,25 @@ class TeleportsWindow(screen: EditorScreen) : Window("Map Teleports") {
     override fun mapChanged(screen: EditorScreen) {
         clearChildren()
         screen.map.map.teleports.forEachIndexed { idx, teleport ->
-            add(VisLabel("$idx: ${WorldMap.of(teleport.map).ident} @ ${teleport.target}")).left().row()
+            add(VisLabel("$idx: ${WorldMap.of(teleport.map).ident} @ ${teleport.target}")).left()
+            val btn = VisTextButton("Remove")
+            add(btn).right().row()
+            btn.onClick {
+                val other = WorldMap.of(teleport.map)
+                other.teleports.removeAt(teleport.target)
+                screen.map.map.teleports.removeAt(idx)
+
+                for (trigger in other.triggers) {
+                    if (trigger.type == TriggerType.Teleport && trigger.idx > teleport.target) trigger.idx--
+                }
+                for (trigger in screen.map.map.triggers) {
+                    if (trigger.type == TriggerType.Teleport && trigger.idx > idx) trigger.idx--
+                }
+
+                screen.mapOrLayoutChanged()
+            }
         }
-        textBtn("Add Teleport Pair") {
+        textBtn("Add Teleport Pair", colspan = 2) {
             Dialogs.showInputDialog(stage, "Enter other map identifier", null, object : InputDialogAdapter() {
                 override fun finished(input: String) {
                     val map = WorldMap.maybeOf(input)
